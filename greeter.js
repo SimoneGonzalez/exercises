b86d75092f6d7030fe3ee917c9aec1252c393745
Simone Gonzalez <simone.b.gonzalez@gmail.com>
2017-01-31 19:20:47 -0600

    additional function

--- greeter.js
+++ greeter.js
@@ -1,19 +1,31 @@
-var name = prompt('What is your name?');
-alert('Hello World! ' + name);
-console.log('Hello World!');
+// var name = prompt('What is your name?');
+// alert('Hello World! ' + name);
+// console.log('Hello World!');
+//
+// var myAge = prompt('How old are you?');
+//
+// var ageCheck = function() {
+//   if(parseInt(myAge) === 24) {
+//     alert('You are awesome!');
+//   }
+//   else if(parseInt(myAge) === 21) {
+//     alert('Meh, you are ok');
+//   }
+//   else {
+//     alert('Too bad');
+//   };
+// };
 
-var myAge = prompt('How old are you?');
+//ageCheck();
 
-var ageCheck = function() {
-  if(parseInt(myAge) === 24) {
-    alert('You are awesome!');
+var myFavoriteFood = prompt('What do you like to eat?');
+
+var getFood = function() {
+  if (myFavoriteFood === 'sushi') {
+    alert('Yay!');
+  } else {
+    alert('maybe next time.');
   }
-  else if(parseInt(myAge === 21)) {
-    alert('Meh, you are ok');
-  }
-  else {
-    alert('Too bad');
-  };
-};
+}
 
-ageCheck();
+getFood();
